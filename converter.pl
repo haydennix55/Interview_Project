686bb6ceb7264f9cba534cf8f05fdf193168322e
Hayden Nix <hayden.nix@colorado.edu>
2016-09-22 17:32:15 -0600

    Update to include XML and conversion options

--- converter.pl
+++ converter.pl
@@ -2,25 +2,59 @@
 
 use strict;
 use warnings;
+use Math::Trig;
+use Math::Round;
  
 open(my $csv, '<', $ARGV[0]);
 
 my $line = <$csv>;
 chomp $line;
-
 my @fields = split "," , $line;
 
-my $json = "{\n";
-while ( $line = <$csv>) {
+
+if ($ARGV[1] eq  "xml"){ #prints xml format
+
+        my $xml = "<XML>\n";
+
+        while ( $line = <$csv>) {
                 chomp $line;
-                $json = $json."{\n";
+                $xml = $xml."\t<object>\n";
                 my @profile = split "," , $line;
 
+                $profile[6] = nearest(0.001,((atan($profile[6]) + (pi/2))*100)/pi); #conversion for courage
+                $profile[7] = nearest(0.001,((atan($profile[7]) + (pi/2))*100)/pi); #conversion for stupidity
+
                 for (my $i = 0; $i < 9; $i++){
-                        $json = $json.$fields[$i].":".$profile[$i].",\n";
+                        $xml = $xml."\t\t<".$fields[$i].">".$profile[$i]."</".$fields[$i].">\n" #string formatting for readability
                 }
 
-                $json =~ s/,$//; #remove trailing comma in object
-                $json = $json."}\n";
+                $xml = $xml."\t</object>\n";
+        }
+
+print $xml."\n</xml>\n";;
+
 }
-print $json . "}\n";
+
+
+
+else { #prints json array format
+
+        my $json = "{\n";
+        while ( $line = <$csv>) {
+                        chomp $line;
+                        $json = $json."{\n";
+                        my @profile = split "," , $line;
+
+                        $profile[6] = nearest(0.001,((atan($profile[6]) + (pi/2))*100)/pi); #conversion for courage
+                        $profile[7] = nearest(0.001,((atan($profile[7]) + (pi/2))*100)/pi); #conversion for stupidity
+
+                        for (my $i = 0; $i < 9; $i++){
+                                $json = $json."\t".$fields[$i].":".$profile[$i].",\n";
+                        }       
+
+                        $json =~ s/,$//; #remove trailing comma in object
+                        $json = $json."}\n";
+        }
+        print $json . "}\n";
+}
+
